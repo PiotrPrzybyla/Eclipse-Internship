367d477be84cba707b7fd5a4c03aaeac3ac98238
Piotr <pepapek@gmail.com>
2022-06-10 16:27:48 +0200

    Step 3

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,14 +2,24 @@ import java.util.ArrayList;
 
 public class Calculator {
     public int Add(String numbers) {
-        String[] strNumbers = (numbers.split(","));
+        String[] commaNumbers = (numbers.split(","));
         ArrayList<Integer> intNumbers = new ArrayList<>();
-
+        ArrayList<String> strNumbers = new ArrayList<>();
+        for (String commaNumber: commaNumbers) {
+            String[] strNumber = commaNumber.split("\n");
+            for (int i = 0; i < strNumber.length; i++) {
+                strNumbers.add(strNumber[i]);
+            }
+        }
         int result = 0;
-        if(numbers != ""){  for (int i = 0; i < strNumbers.length; i++) {
-            intNumbers.add(Integer.parseInt(strNumbers[i]));
-            result+=intNumbers.get(i);
-        }}
+        if(numbers != ""){
+            for (String strNumber: strNumbers) {
+                intNumbers.add(Integer.parseInt(strNumber));
+            }
+            for (int number: intNumbers) {
+                result+=number;
+            }
+        }
 
         return result;
 
--- src/test/java/ProgramTest.java
+++ src/test/java/ProgramTest.java
@@ -5,17 +5,21 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class ProgramTest {
+    Calculator calc = new Calculator();
     @Test
     public void step1Test(){
-        Calculator calc = new Calculator();
         assertEquals(3,calc.Add("1,2"));
         assertEquals(2,calc.Add("2"));
         assertEquals(0,calc.Add(""));
     }
     @Test
     public void step2Test(){
-        Calculator calc = new Calculator();
         assertEquals(6,calc.Add("1,2,3"));
         assertEquals(11,calc.Add("1,2,3,5"));
+    }
+    @Test
+    public void step3Test(){
+        assertEquals(6,calc.Add("1\n2,3"));
+        assertEquals(6,calc.Add("1\n2\n3"));
     }
 }
